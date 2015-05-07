5b08905952add79169b6584150f48f0cf0a22a74
johan <airess2004@yahoo.com>
2015-05-07 16:19:53 +0700

    as

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,84 +1,66 @@
 dataSource {
-<<<<<<< HEAD
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.postgresql.Driver"
-    username = "webadmin"
-    password = "HXCqsn83750"
-=======
-    
->>>>>>> 4d5cb98b4e0ac6cf0151cc85df5c6fc97fd6bc21
+	pooled = true
+	jmxExport = true
+	//	username = "webadmin"
+	//	password = "DZGpya11811"
+
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
-    singleSession = true // configure OSIV singleSession mode
-    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+	cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+	singleSession = true // configure OSIV singleSession mode
+	flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+	development {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:postgresql://localhost:5432/estatemanagement"
+			driverClassName = "org.postgresql.Driver"
+			username = "postgres"
+			password = "123"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "create-drop"
+			url = "jdbc:postgresql://localhost:5432/estatemanagement"
+			driverClassName = "org.postgresql.Driver"
+			username = "postgres"
+			password = "123"
+		}
+	}
+	production {
+		dataSource {
 			dbCreate = "update"
 			url = "jdbc:postgresql://localhost:5432/estatemanagement"
-			pooled = true
-			jmxExport = true
 			driverClassName = "org.postgresql.Driver"
 			username = "postgres"
 			password = "123"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-<<<<<<< HEAD
-            url = "jdbc:postgresql://localhost:5432/estatemanagement"
-        }
-=======
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-			pooled = true
-			jmxExport = true
+			properties {
+				// See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+				jmxEnabled = true
+				initialSize = 5
+				maxActive = 50
+				minIdle = 5
+				maxIdle = 25
+				maxWait = 10000
+				maxAge = 10 * 60000
+				timeBetweenEvictionRunsMillis = 5000
+				minEvictableIdleTimeMillis = 60000
+				validationQuery = "SELECT 1"
+				validationQueryTimeout = 3
+				validationInterval = 15000
+				testOnBorrow = true
+				testWhileIdle = true
+				testOnReturn = false
+				jdbcInterceptors = "ConnectionState"
+				defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
 			}
->>>>>>> 4d5cb98b4e0ac6cf0151cc85df5c6fc97fd6bc21
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-<<<<<<< HEAD
-			
-            url = "jdbc:postgresql://postgres20973-estate.jelastic.skali.net/estatemanagement"
-=======
-            url = "jdbc:postgresql://localhost:5432/estatemanagement"
-			pooled = true
-			jmxExport = true
-			driverClassName = "org.postgresql.Driver"
-			username = "postgres"
-			password = "123"
->>>>>>> 4d5cb98b4e0ac6cf0151cc85df5c6fc97fd6bc21
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
-        }
-    }
+		}
+	}
 }
